96e001207b9a98df9b49ef240abac40189688e4c
DawidSkowronski <282241@student.pwr.edu.pl>
2026-04-29 10:57:36 +0200

    Dodanie historii do algorytmu G-N

--- Alg_Gauss_Newton_kody.R
+++ Alg_Gauss_Newton_kody.R
@@ -73,21 +73,15 @@ G_N_rozw <- G_N(x,y_gen)
 beta
 
 ## Zadanie 4
-# Prezentacja w tabeli
+# Prezentacja w tabeli kroków 0,1,2 i ostatniego
 
 G_N_tab <- function(x,y, beta_0 = c(8,2,1), eps=0.0001 , M=100){
   
   n <- length(x)
   p <- length(beta_0)
   
-  historia <- data.frame(
-    krok = 0,
-    beta1=0,
-    beta2=0,
-    beta3=0,
-    sigma_kw=0,
-    L=0
-  )
+  historia <- data.frame( )
+  
   
   # Funkcja która wylicza w każdym kroku informacje
   # Zapisanie tego do ramki danych
@@ -99,8 +93,20 @@ G_N_tab <- function(x,y, beta_0 = c(8,2,1), eps=0.0001 , M=100){
     y_prog <- fun_g(x, stare_beta)
     G <- mac_G(x, beta = stare_beta)
     
+    
     krok <- solve(t(G) %*% G) %*%
       t(G)%*%(y-y_prog)
+    
+    
+    RSS <- sum((y-y_prog)^2)
+    L_log <- -n/2*log(2*pi) - n/2 * log(RSS/(n-p)) - (n-p)/2
+    
+    historia <- rbind(historia, data.frame( krok = i,
+                                            beta1 = stare_beta[1],
+                                            beta2 = stare_beta[2],
+                                            beta3 = stare_beta[3],
+                                            sigma_kw = RSS/(n-p),
+                                            L = L_log))
     
     nowe_beta <- stare_beta + krok
     
@@ -116,8 +122,13 @@ G_N_tab <- function(x,y, beta_0 = c(8,2,1), eps=0.0001 , M=100){
   RSS <- sum((y - y_koniec)^2)
   sig_kw <- RSS / (n - p)
   
-  return(list(beta_est = nowe_beta, sigma2_est = sig_kw, iteracje = i))
+  return(list(beta_est = nowe_beta, sigma2_est = sig_kw, iteracje = i, historia))
 }
+
+# Algorytm z historią
+G_N_tab(x,y_gen)
+
+
 
 # Zadanie 6
 y_pred <- fun_g(x, G_N_rozw$beta_est)
